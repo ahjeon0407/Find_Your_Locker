c8cdfbc7792b755d35cd67ffba828ce48d6bd22e
ahjeon0407 <ahyeon0707@naver.com>
2022-11-22 23:58:50 +0900

    [FIX] Fix Typo

--- android/app/src/main/java/com/project/locker/MainActivity.java
+++ android/app/src/main/java/com/project/locker/MainActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
